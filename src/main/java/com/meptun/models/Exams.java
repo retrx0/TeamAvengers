deb89888bcfd7d15c3842c86a677222e80766618
Mav-lit <ngonie3mav@gmail.com>
2020-04-11 16:54:16 +0200

    @ annotations

--- src/main/java/com/meptun/models/Exams.java
+++ src/main/java/com/meptun/models/Exams.java
@@ -5,18 +5,36 @@
  */
 package com.meptun.models;
 
+import java.io.Serializable;
 import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.hibernate.annotations.Generated;
 
 /**
  *
  * @author ngoni
  */
-public class Exams {
+@Entity
+@Table(name = "Exams")
+public class Exams implements Serializable{
+    int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "course")
     private Course course;
+    @Column(name = "dateOfExam")
     private LocalDate dateOfExam;
+    @Column(name = "headCount")
     private int headCount;
+    @Column(name = "room")
     private String room;
-
+   
+    
     public Exams(Course course, LocalDate dateOfExam, int headCount, String room) {
         this.course = course;
         this.dateOfExam = dateOfExam;
